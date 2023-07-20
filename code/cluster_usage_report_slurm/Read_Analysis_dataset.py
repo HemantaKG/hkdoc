1e2c594e8f1cb0afc19c5e46739aa04672d2f91f
HemantaKG <hemant.kumar1704@gmail.com>
2023-07-21 00:44:10 +0530

    added new project cluster usage report geneartion using slurm data

--- code/cluster_usage_report_slurm/Read_Analysis_dataset.py
+++ code/cluster_usage_report_slurm/Read_Analysis_dataset.py
@@ -6,7 +6,6 @@ import seaborn as sns
 
 # Define a custom function to convert seconds to "days-HH:MM:SS" format
 def seconds_to_time_str(total_seconds):
-    # print(total_seconds)
     days, remainder = divmod(total_seconds, 86400)
     hours, remainder = divmod(remainder, 3600)
     minutes, seconds = divmod(remainder, 60)
@@ -14,9 +13,7 @@ def seconds_to_time_str(total_seconds):
 
 # Function to convert a time string in "days-HH:MM:SS" format to seconds
 def time_str_to_seconds(time_str):
-    # print(time_str)
     time_components = time_str.split('-')
-    # print(time_components)
 
     if len(time_components) == 2:
         days = int(time_components[0])
@@ -68,12 +65,12 @@ def convert_time_seconds(selected_rows_copy, column_header):
     return selected_rows_copy[column_header].apply(time_str_to_seconds)
 
 def plot_frequency_job_partition(selected_rows, title, xlabel, ylabel):
-    # PART 0: Find the frequency of each Partition
+    # Find the frequency of each Partition
     partition_frequency = selected_rows['Partition'].value_counts()
 
-    # Print the user frequency
-    print("Partition Frequency:")
-    print(partition_frequency)
+    # # Print the user frequency
+    # print("Partition Frequency:")
+    # print(partition_frequency)
 
     # Set the Seaborn style to include gridlines
     sns.set_style('whitegrid')
@@ -95,9 +92,13 @@ def plot_frequency_job_partition(selected_rows, title, xlabel, ylabel):
 def plot_reqcpus_frequency(selected_rows_copy, title, xlabel, ylabel):
     reqCPU_frequency= selected_rows_copy['ReqCPUS'].value_counts()
 
-    # Print ReqCPUs freqyency
-    print("ReqCPUs frequency:")
-    print(reqCPU_frequency)
+    # Sort the reqCPU_frequency Series in ascending order by index (which contains the ReqCPUS values)
+    reqCPU_frequency.index = reqCPU_frequency.index.astype(int)
+    reqCPU_frequency = reqCPU_frequency.sort_index()
+
+    # # Print ReqCPUs freqyency
+    # print("ReqCPUs frequency:")
+    # print(reqCPU_frequency)
 
     # Set the Seaborn style to include gridlines
     sns.set_style('whitegrid')
@@ -119,9 +120,12 @@ def plot_reqcpus_frequency(selected_rows_copy, title, xlabel, ylabel):
 def plot_user_frequency(selected_rows_copy, title, xlabel, ylabel):
     user_frequency = selected_rows_copy['User'].value_counts()
 
-    # Print the user frequency
-    print("User Frequency:")
-    print(user_frequency)
+    # Sort the user_frequency Series in ascending order by index (which contains the User values)
+    user_frequency = user_frequency.sort_index()
+
+    # # Print the user frequency
+    # print("User Frequency:")
+    # print(user_frequency)
 
     # Set the Seaborn style to include gridlines
     sns.set_style('whitegrid')
@@ -159,9 +163,9 @@ def plot_reqmem_bin_count(selected_rows_copy, title, xlabel, ylabel):
     # Count the occurrences of each bin label
     bin_counts = selected_rows_copy['ReqMem Bin'].value_counts()
 
-    # Print the counts for each bin
-    print("Bin Counts:")
-    print(bin_counts)
+    # # Print the counts for each bin
+    # print("Bin Counts:")
+    # print(bin_counts)
 
     # Set the Seaborn style to include gridlines
     sns.set_style('whitegrid')
@@ -178,6 +182,7 @@ def plot_reqmem_bin_count(selected_rows_copy, title, xlabel, ylabel):
     plt.yscale('log')
     # Enable grid for the x-axis
     plt.grid(axis='x')
+    plt.tight_layout()  # Adjust the spacing between the bars and the plot borders
     plt.show()
 
 def plot_elapsed_bin_count(selected_rows_copy, bin_edges, bin_labels, title, xlabel, ylabel):
@@ -190,9 +195,9 @@ def plot_elapsed_bin_count(selected_rows_copy, bin_edges, bin_labels, title, xla
     # Count the occurrences of each bin label
     bin_counts = selected_rows_copy['Elapsed Bin'].value_counts()
 
-    # Print the counts for each bin
-    print("Bin Counts:")
-    print(bin_counts)
+    # # Print the counts for each bin
+    # print("Bin Counts:")
+    # print(bin_counts)
 
     # Set the Seaborn style to include gridlines
     sns.set_style('whitegrid')
@@ -209,9 +214,10 @@ def plot_elapsed_bin_count(selected_rows_copy, bin_edges, bin_labels, title, xla
     plt.yscale('log')
     # Enable grid for the x-axis
     plt.grid(axis='x')
+    plt.tight_layout()  # Adjust the spacing between the bars and the plot borders
     plt.show()
 
-def total_cputime_per_user(selected_rows):
+def calculate_cputime_per_user(selected_rows):
     # Group the DataFrame by 'User' and calculate the total 'CPUTime' for each user
     total_cputime_per_user = selected_rows.groupby('User')['CPUTime'].sum()
 
@@ -220,7 +226,7 @@ def total_cputime_per_user(selected_rows):
 
     return total_cputime_per_user
 
-def total_elapsedtime_per_user(selected_rows):
+def calculate_elapsedtime_per_user(selected_rows):
     # Group the DataFrame by 'User' and calculate the total 'ElapsedTime' for each user
     total_elapsedtime_per_user = selected_rows.groupby('User')['Elapsed'].sum()
 
@@ -229,33 +235,55 @@ def total_elapsedtime_per_user(selected_rows):
 
     return total_elapsedtime_per_user
 
+def plot_elapsedtime_cputime_per_user(selected_rows_copy):
+    total_cputime_per_user = selected_rows_copy.groupby('User')['CPUTime'].sum()
+    total_elapsedtime_per_user = selected_rows_copy.groupby('User')['Elapsed'].sum()
 
-def plot_elapsedtime_cputime_per_user(total_cputime_per_user, total_elapsedtime_per_user, title, xlabel, ylabel):
-    # Convert the total CPUTime back to "days-HH:MM:SS" format
-    # total_cputime_per_user_str = total_cputime_per_user.apply(seconds_to_time_str)
+    # Create two subplots (side by side)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
 
-    # Create a new DataFrame with the total CPUTime and total ElapsedTime for each user
-    user_times_df = pd.DataFrame({'Total CPUTime': total_cputime_per_user,
-                                'Total ElapsedTime': total_elapsedtime_per_user})
+    # Plot the total CPUTime for each user using a line chart with log scale and solid dots
+    sns.lineplot(x=total_cputime_per_user.index, y=total_cputime_per_user.values, dashes=False, ax=ax1)
+    sns.scatterplot(x=total_cputime_per_user.index, y=total_cputime_per_user.values, color='blue', marker='o', s=30, ax=ax1)
+    ax1.set_xlabel('User')
+    ax1.set_ylabel('Total CPUTime (seconds)')
+    ax1.set_title(f'Total CPUTime of Each User {ncpu} [Year: {year}]')
+    ax1.set_xticks(range(len(total_cputime_per_user.index)))
+    ax1.set_xticklabels(total_cputime_per_user.index, rotation=45, ha='right')
+    ax1.set_yscale('log')
+    ax1.grid(True)
 
-    # Sort the DataFrame by the Total CPUTime (optional, you can remove this line if you don't want sorting)
-    user_times_df = user_times_df.sort_values(by='Total CPUTime', ascending=False)
+    # Annotate each data point on the plot with its corresponding value
+    for x, y in zip(total_cputime_per_user.index, total_cputime_per_user.values):
+        # ax1.text(x, y, f'{y:.2e}', ha='left', va='center', fontsize=8, rotation=90)
+        ax1.text(x, y, f'{seconds_to_time_str(y)}', ha='right', va='center', fontsize=8, rotation=90)
+    
+    # Plot the total ElapsedTime for each user using a line chart with log scale and solid dots
+    sns.lineplot(x=total_elapsedtime_per_user.index, y=total_elapsedtime_per_user.values, dashes=False, ax=ax2)
+    sns.scatterplot(x=total_elapsedtime_per_user.index, y=total_elapsedtime_per_user.values, color='blue', marker='o', s=30, ax=ax2)
+    ax2.set_xlabel('User')
+    ax2.set_ylabel('Total ElapsedTime (seconds)')
+    ax2.set_title(f'Total ElapsedTime of Each User {ncpu} [Year: {year}]')
+    ax2.set_xticks(range(len(total_elapsedtime_per_user.index)))
+    ax2.set_xticklabels(total_elapsedtime_per_user.index, rotation=45, ha='right')
+    ax2.set_yscale('log')
+    ax2.grid(True)
 
-    # Plot the total CPUTime and total ElapsedTime for each user using a line chart
-    plt.figure(figsize=(10, 6))
-    sns.lineplot(data=user_times_df, markers=True, dashes=False)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
-    plt.xticks(rotation=45, ha='right')
+    # Annotate each data point on the plot with its corresponding value
+    for x, y in zip(total_elapsedtime_per_user.index, total_elapsedtime_per_user.values):
+        # ax2.text(x, y, f'{y:.2e}', ha='left', va='center', fontsize=8, rotation=90)
+        ax2.text(x, y, f'{seconds_to_time_str(y)}', ha='right', va='center', fontsize=8, rotation=90)
+
     plt.tight_layout()
-    plt.legend(loc='upper left')
-    plt.grid(True)
     plt.show()
 
 
+
 # START
+# Data of the Year
 year= 2021
+# Number of CPU 'eq 1' or 'gt 1'
+ncpu= 'gt 1'
 
 # Read the data from the file.txt file
 filename = '2021/dt20210101-20211231.txt'
@@ -275,15 +303,15 @@ df = pd.DataFrame(data, columns=headers)
 # print(df)
 
 # Select rows where 'ReqCPUS' is 1 and 'State' is COMPLETED and 'Partition' isn't devel
-selected_rows = df[(df['ReqCPUS'] == '1') & (df['State'] == 'COMPLETED') & (df['Partition'] != "devel")]
+selected_rows = df[(df['ReqCPUS'] != '1') & (df['State'] == 'COMPLETED') & (df['Partition'] != "devel")]
 
 # Print the selected data
 # print(selected_rows)
 
-# PART 0: Find the frequency of each Job in Partition
+# PART 0.1: Find the frequency of each Job in Partition
 xlabel = 'Partition'
-ylabel = 'Frequency'
-title = 'Partion Frequency of ReqCPU eq 1 [Year: '+ str(year) +']'
+ylabel = 'Job Count'
+title = (f'Job Frequency of Partition of ReqCPU {ncpu} [Year: {year}]')
 plot_frequency_job_partition(selected_rows, title, xlabel, ylabel)
 
 # Create a copy of the selected_rows DataFrame
@@ -297,69 +325,64 @@ selected_rows_copy['Elapsed']= convert_time_seconds(selected_rows_copy, 'Elapsed
 selected_rows_copy['CPUTime']= convert_time_seconds(selected_rows_copy, 'CPUTime')
 # print(selected_rows_copy['CPUTime'])
 
-
-# PART 0: Find the Total CPU Time oer User
-total_cputime_per_user= total_cputime_per_user(selected_rows_copy)
+# Find the Total CPU Time oer User
+total_cputime_per_user= calculate_cputime_per_user(selected_rows_copy)
 
 # Print the total ElapsedTime for each user
 # print("Total CPUTime per User:")
 # print(total_cputime_per_user)
 
-# PART 0: Find the Total Elapsed Time oer User
-total_elapsedtime_per_user= total_elapsedtime_per_user(selected_rows_copy)
+# Find the Total Elapsed Time oer User
+total_elapsedtime_per_user= calculate_elapsedtime_per_user(selected_rows_copy)
 
 # Print the total ElapsedTime for each user
 # print("Total ElapsedTime per User:")
 # print(total_elapsedtime_per_user)
 
-# Plot the total CPUTime and total ElapsedTime for each user using a line chart
-xlabel = 'User'
-ylabel = 'Time (days-HH:MM:SS)'
-title = 'Total CPUTime and ElapsedTime of Each User [Year: '+ str(year) +']'
-# plot_elapsedtime_cputime_per_user(total_cputime_per_user, total_elapsedtime_per_user,title, xlabel, ylabel)
+# Plot 0.2: Plot total CPUTime and total ElapsedTime for each user using a line/scatter chart
+plot_elapsedtime_cputime_per_user(selected_rows_copy)
+
 
 # PART 1: Great Then Then 1 Day Elapsed Time Jobs
-
 # Create a copy of the selected_rows DataFrame
 selected_rows_copy_gt1 = selected_rows_copy.copy()
 
 # Filter rows where the 'Elapsed' column is greater than or equal to 1 day (86400 seconds)
-selected_rows_copy_gt1 = selected_rows_copy_gt1[selected_rows_copy_gt1['Elapsed'] >= 86400.0]
+selected_rows_copy_gt1 = selected_rows_copy_gt1[selected_rows_copy_gt1['Elapsed'] > 86400.0]
 
 # Print the DataFrame with the modified 'ReqMem' column
 # print(selected_rows_copy_gt1)
 
 # PART 1.1: Find the frequency of each Job Elapsed Time of job Elapsed time gt 24H
-xlabel = 'Time Bin'
-ylabel = 'Count'
-title = 'Job Count of Elapsed Time gt 24H and ReqCPU eq 1 [Year: '+ str(year) +']'
 # Define the bin edges for the 'Elapsed' column (up to 30 days)
 bin_edges = np.arange(0, 30*24*60*60 + 1, 86400)
 # Define the labels for each bin range
 bin_labels = [f'{i}-{i+1} days' for i in range(30)]
+xlabel = 'Time Bin'
+ylabel = 'Job Count'
+title = (f'Job Count of Elapsed Time gt 24h and ReqCPU {ncpu} [Year: {year}]')
 plot_elapsed_bin_count(selected_rows_copy_gt1, bin_edges, bin_labels, title, xlabel, ylabel)
 
 # PART 1.2: Find the frequency of each Job ReqMem of job Elapsed time gt 24H
 xlabel = 'ReqMem Bin'
-ylabel = 'Count'
-title = 'Job Count of ReqMem bins Elapsed Time gt 24H and ReqCPU eq 1 [Year: '+ str(year) +']'
+ylabel = 'Job Count'
+title = (f'Job Count of ReqMem bins Elapsed Time gt 24h and ReqCPU {ncpu} [Year: {year}]')
 plot_reqmem_bin_count(selected_rows_copy_gt1, title, xlabel, ylabel)
 
 # PART 1.3: Find the frequency of each User of job Elapsed time gt 24H
 xlabel = 'User'
 ylabel = 'Frequency'
-title = 'User Frequency of Elapsed Time gt 24H and ReqCPU eq 1 [Year: '+ str(year) +']'
+title = (f'User Frequency of Elapsed Time gt 24h and ReqCPU {ncpu} [Year: {year}]')
 plot_user_frequency(selected_rows_copy_gt1, title, xlabel, ylabel)
 
 # PART 1.4: Find the frequency of each ReqCPUs of job Elapsed time gt 24H
 xlabel= 'ReqCPUs'
 ylabel= 'Frequency'
-title = 'ReqCPUs Frequency of gt 24H ReqCPU eq 1[Year: '+ str(year) +']'
+title = (f'ReqCPUs Frequency of gt 24h ReqCPU {ncpu} [Year: {year}]')
 plot_reqcpus_frequency(selected_rows_copy_gt1, title, xlabel, ylabel)
 
 
 # PART 2: Less Then 1 Day Elapsed Time Jobs
-
 # Create a copy of the selected_rows DataFrame
 selected_rows_copy_lt1 = selected_rows_copy.copy()
 
@@ -373,31 +396,31 @@ selected_rows_copy_lt1 = selected_rows_copy_lt1[(selected_rows_copy_lt1['Elapsed
 # print(selected_rows_copy_lt1)
 
 # PART 2.1: Find the frequency of each Job Elapsed Time of job Elapsed time lt 24H
-xlabel = 'Time Bin'
-ylabel = 'Count'
-title = 'Job Count of Elapsed Time lt 24H and ReqCPU eq 1 [Year: '+ str(year) +']'
 # Define the bin edges for the 'Elapsed' column (up to 24H)
 bin_edges = np.arange(0, 24*60*60 + 1, 60*60)
 # Define the labels for each bin range
 bin_labels = [f'{i}-{i+1} H' for i in range(24)]
+xlabel = 'Time Bin'
+ylabel = 'Job Count'
+title = (f'Job Count of Elapsed Time lt 24h and ReqCPU {ncpu} [Year: {year}]')
 plot_elapsed_bin_count(selected_rows_copy_lt1, bin_edges, bin_labels, title, xlabel, ylabel)
 
 # PART 2.2: Find the frequency of each Job ReqMem of job Elapsed time lt 24H 
 xlabel = 'ReqMem Bin'
-ylabel = 'Count'
-title = 'Job Count of ReqMem bins Elapsed Time lt 24H and ReqCPU eq 1 [Year: '+ str(year) +']'
+ylabel = 'Job Count'
+title = (f'Job Count of ReqMem bins Elapsed Time lt 24h and ReqCPU {ncpu} [Year: {year}]')
 plot_reqmem_bin_count(selected_rows_copy_lt1, title, xlabel, ylabel)
 
 # PART 2.3: Find the frequency of each User of job Elapsed time lt 24H
 xlabel = 'User'
 ylabel = 'Frequency'
-title = 'User Frequency of Elapsed Time lt 24H and ReqCPU eq 1 [Year: '+ str(year) +']'
+title = (f'User Frequency of Elapsed Time lt 24h and ReqCPU {ncpu} [Year: {year}]')
 plot_user_frequency(selected_rows_copy_lt1, title, xlabel, ylabel)
 
 # PART 2.4: Find the frequency of each ReqCPUs of job Elapsed time lt 24H
 xlabel= 'ReqCPUs'
 ylabel= 'Frequency'
-title = 'ReqCPUs Frequency of lt 24H ReqCPu eq 1[Year: '+ str(year) +']'
+title = (f'ReqCPUs Frequency of lt 24h ReqCPU {ncpu} [Year: {year}]')
 plot_reqcpus_frequency(selected_rows_copy_lt1, title, xlabel, ylabel)
 
 
